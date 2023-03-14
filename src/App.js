2d17a38de81ea959ab5635305afb7a02c0874640
sunnyvk <khambayatsunny278@gmail.com>
2023-03-14 21:20:26 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+<div>
+
+  <Main />
+  
+    <Navbar />
+</div>
+  
+  );
+}
+
+export default App;
